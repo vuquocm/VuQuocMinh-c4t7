087919f5c2fcfa0692c6dec49e1128f74107efb5
vuquocm <vuquocminh2002@gmail.com>
2018-10-03 19:26:09 +0700

    .

--- hw.py
+++ hw.py
@@ -1,14 +1,15 @@
 from turtle import *
 
-
-left(120)
-for j in range(4):
-    for i in range(2):
-        forward(100)
-        right(60)
-        forward(100)
-        right(120)
-    right(90)
+for x in range(2):
+    left(120)
+    for j in range(4):
+        for i in range(2):
+            forward(100)
+            right(60)
+            forward(100)
+            right(120)
+        right(90)
+    left(15)
 
 
 
